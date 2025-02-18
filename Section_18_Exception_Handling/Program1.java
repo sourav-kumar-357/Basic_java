50f2c9b126f558f9b0350ab7e83fa9799be0c853
Sourav Kumar <xsouravkumar357@gmail.com>
2025-02-18 17:03:59 +0530

    updated the program1

--- Section_18_Exception_Handling/Program1.java
+++ Section_18_Exception_Handling/Program1.java
@@ -4,10 +4,17 @@ public class Program1 {
 
     public static void main(String[] args) {
 
-        int a = 10, b = 2, c;
-        c = a / b;
+        try {
+            int a = 10, b = 2, c;
+            c = a / b;
 
-        System.out.println(c);
+            System.out.println(c);
+
+        } catch (ArithmeticException e) {
+
+            // TODO: handle exception
+            System.out.println("Denominator cannot be Zero");
+        }
         System.out.println("bye");
     }
 }
