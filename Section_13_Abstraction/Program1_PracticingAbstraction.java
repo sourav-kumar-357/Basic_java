640fa029dbe1b4161313f199c8109c554239107e
Sourav Kumar <xsouravkumar357@gmail.com>
2025-02-21 23:15:58 +0530

    The Unchecked Exception

--- Section_13_Abstraction/Program1_PracticingAbstraction.java
+++ Section_13_Abstraction/Program1_PracticingAbstraction.java
@@ -3,10 +3,12 @@ package Section_13_Abstraction;
 abstract class Super { // making an abstract class
     public Super() {
         System.out.println("Super class constructor");
-    }    
+    }
+
     public void meth1() {
         System.out.println("Method one of super class");
     }
+
     abstract public void meth2();
 }
 
@@ -14,6 +16,7 @@ class Sub extends Super {
     public Sub() {
         System.out.println("This is the sub Class");
     }
+
     @Override
     public void meth2() {
         System.out.println("This is an abstract method");
@@ -22,7 +25,8 @@ class Sub extends Super {
 
 public class Program1_PracticingAbstraction {
     public static void main(String[] args) {
-        // Super s1 = new Super(); // Showing an error as we cannot make object of abstract class
+        // Super s1 = new Super(); // Showing an error as we cannot make object of
+        // abstract class
         // s1.meth1();
 
         Super s2 = new Sub();
---
+++ Section_18_Exception_Handling/Program5.java
@@ -0,0 +1,32 @@
+// The Checked Exceptions and their handling process
+
+// The origin class of the program
+public class Program5 {
+
+    // Creating some anonymous functions here
+    static void fun1() {
+        // FileInputStream f1 = new FileInputStream("My.txt"); // Now this is is an
+        // UnChecked exception and the compiler is
+        // showing the exception and we have to handle it whatsoever
+        // to run the program
+    }
+
+    // Creating the same type of function
+    static void fun2() {
+        fun1();
+    }
+
+    // Function part 3 to access the top one
+    static void fun3() {
+        fun2();
+    }
+
+    // The main class of the program is here
+    public static void main(String[] args) {
+
+        // calling the herirachy of functions
+        fun3();
+
+        System.out.println("Exiting the program");
+    }
+}
